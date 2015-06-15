1b8b9e998d6317625a40b1d96142210aef993e86
Leon Waldman <le.waldman@gmail.com>
2015-06-14 21:06:55 -0300

    defaults: Added test recipe

---
+++ defaults/recipes/test.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: defaults
+# Recipe:: test
+#
+# Copyright (C) 2015 Leon Waldman
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# Includes
+include_recipe 'apt'
+
+# Packages
+package 'apache2'
